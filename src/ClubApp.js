cecaa29cdb9690633c716844d3b05cd235beaeeb
Arthur <arthur.robieux@gmail.com>
2018-12-28 09:56:39 +0100

    Create general ClubApp

---
+++ src/ClubApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import CalendarApp from './Calendar/CalendarApp';
+
+class ClubApp extends Component {
+
+    render() {
+        return (
+            <CalendarApp/>
+        );
+    }
+}
+
+export default ClubApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CalendarApp from './CalendarApp';
+import ClubApp from './ClubApp';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CalendarApp />, document.getElementById('root'));
+ReactDOM.render(<ClubApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
