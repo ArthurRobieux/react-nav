f686119959250da577cb61d710a57ff03c041971
Arthur <arthur.robieux@gmail.com>
2018-12-28 11:22:27 +0100

    Fix SideHeader style

--- src/SideHeader/SideHeader.js
+++ src/SideHeader/SideHeader.js
@@ -17,31 +17,31 @@ class SideHeader extends Component {
 
                    <nav className="main-menu">
 
-                        <img src="img/logo.png" id="logo"/>
+                        <img className={"side_header_image"} src="img/logo.png" id="logo"/>
 
                         <ul>
                             <li>
                                 <Link to="/roster">
-                                    <i className="fa fa-members"><img src="img/members.png" id="members"/></i>
+                                    <i className="fa fa-members"><img className={"side_header_image"} src="img/members.png" id="members"/></i>
                                     <span className="nav-text">Membres</span>
                                 </Link>
                             </li>
                             <li>
                                 <Link to="/calendar">
-                                    <i className="fa fa-members"><img src="img/calendar.png" id="members"/></i>
+                                    <i className="fa fa-members"><img className={"side_header_image"} src="img/calendar.png" id="members"/></i>
                                     <span className="nav-text">Calendrier</span>
                                 </Link>
                             </li>
                             <li>
                                 <a href="#">
-                                    <i className="fa fa-teams"><img src="img/teams.png" id="teams"/></i>
+                                    <i className="fa fa-teams"><img className={"side_header_image"} src="img/teams.png" id="teams"/></i>
                                     <span className="nav-text">Equipes</span>
                                 </a>
 
                             </li>
                             <li>
                                 <a href="#">
-                                    <i className="fa fa-settings"><img src="img/settings.png" id="settings"/></i>
+                                    <i className="fa fa-settings"><img className={"side_header_image"} src="img/settings.png" id="settings"/></i>
                                     <span className="nav-text">Paramètres</span>
                                 </a>
                             </li>
@@ -51,14 +51,14 @@ class SideHeader extends Component {
                         <ul className="logout">
                             <li>
                                 <a href="#">
-                                    <i className="fa fa-home"><img src="img/logout.png" id="logout"/></i>
+                                    <i className="fa fa-home"><img className={"side_header_image"} src="img/logout.png" id="logout"/></i>
                                     <span className="nav-text">Se déconnecter</span>
                                 </a>
                             </li>
                         </ul>
 
                     </nav>
-                    
+
                     <Route path="/calendar" component={CalendarApp} />
                     <Route path="/roster" component={MembersListApp} />
                 </div>
